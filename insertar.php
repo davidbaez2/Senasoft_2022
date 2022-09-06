3a7d082043cd865aa34c66bb35a6905f56408cc0
davidbaez2 <112959069+davidbaez2@users.noreply.github.com>
2022-09-06 18:46:47 -0500

    Delete insertar.php

--- insertar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conetar.php';
-$tipo_documento = $_POST[""]
-$numero_de_documento = $_POST[""]
-$nombres_completos = $_POST[""]
-$apellidos = $_POST[""]
-$tipo_documento = $_POST[""]
-$guardar = mysqli_query($conetar,"INSERT INTO identificacion('')")
-?>
